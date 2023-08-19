74da367513fdb3935cb2128a8e3eda292e2998aa
restaurent999 <gudurisandeep999@gmail.com>
2023-08-19 16:35:28 +0530

    third baanch commit

--- index.js
+++ index.js
@@ -14,4 +14,8 @@ app.get('/two', (req, res) => {
     res.json({ response: "first route" })
 })
 
+app.get('/three', (req, res) => {
+    res.json({ response: "third route" })
+})
+
 app.listen(8000, () => { console.log("port is running in 8000") })
